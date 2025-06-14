55439ed6104862c51f24007974a446c1cafb0090
Awais Ahmad Nasar <awaisbiit4415@gmail.com>
2025-06-14 10:34:18 +0500

    Ignore Android .cxx build files

--- android/app/.cxx/Debug/704z6q13/prefab/armeabi-v7a/prefab/lib/arm-linux-androideabi/cmake/react-native-vision-camera/react-native-vision-cameraConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET react-native-vision-camera::VisionCamera)
-add_library(react-native-vision-camera::VisionCamera SHARED IMPORTED)
-set_target_properties(react-native-vision-camera::VisionCamera PROPERTIES
-    IMPORTED_LOCATION "D:/ReactNativeProject/CricketCoach/node_modules/react-native-vision-camera/android/build/intermediates/cxx/Debug/1d66441d/obj/armeabi-v7a/libVisionCamera.so"
-    INTERFACE_INCLUDE_DIRECTORIES "D:/ReactNativeProject/CricketCoach/node_modules/react-native-vision-camera/android/build/headers/visioncamera"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
